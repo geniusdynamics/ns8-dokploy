0211ae7e8cd9c17a80e73f4130d716677aeff266
Martin Bhuong <martin@genius.ke>
2025-09-16 20:42:22 +0300

    Update to version 0.25.2

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="dokploy"
-dokploy_version="v0.25.0"
+dokploy_version="v0.25.2"
 
 # Create a new empty container image
 container=$(buildah from scratch)
